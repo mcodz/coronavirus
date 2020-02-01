be1ec4abc3319cc6b2a700144e6426affa519f29
Mcodz <hkmicc@gmail.com>
2020-02-01 13:11:59 +0800

    update

---
+++ data.js
@@ -0,0 +1,151 @@
+// https://www.chp.gov.hk/files/pdf/building_list_chi.pdf
+
+var geojsonFeature = [
+
+{
+    "type": "Feature",
+    "properties": {
+        "desc_c": "沙田 帝逸酒店 ",
+        "desc_e": "Shatin Alva Hotel by Royal",
+        "date": "20/1/2020"
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [114.2060521, 22.3835259]
+    }
+},
+
+{
+    "type": "Feature",
+    "properties": {
+        "desc_c": "沙田 天宇海 1A 座",
+        "desc_e": "Shatin Tower 1A, Oceanaire",
+        "date": "21/1/2020"
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [114.2208166, 22.4151657]
+    }
+},
+
+{
+    "type": "Feature",
+    "properties": {
+        "desc_c": "沙田 銀湖天峰第一座",
+        "desc_e": "Shatin Tower 1, Lake Silver",
+        "date": "23/1/2020"
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [114.2416176, 22.4287385]
+    }
+},
+
+{
+    "type": "Feature",
+    "properties": {
+        "desc_c": "屯門 倚嶺南庭",
+        "desc_e": "Tuen Mun South Hillcrest",
+        "date": "24/1/2020"
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [113.9815208, 22.412407]
+    }
+},
+
+{
+    "type": "Feature",
+    "properties": {
+        "desc_c": "葵青 長康邨康美樓",
+        "desc_e": "Kwai Tsing Hong Mei House, Cheung Hong Estate",
+        "date": "24/1/2020"
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [114.0971083, 22.350361]
+    }
+},
+
+{
+    "type": "Feature",
+    "properties": {
+        "desc_c": "東區 宇宙閣",
+        "desc_e": "Eastern Universal Towers",
+        "date": "25/1/2020"
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [114.195135, 22.2892024]
+    }
+},
+
+
+{
+    "type": "Feature",
+    "properties": {
+        "desc_c": "西貢區 將軍澳廣場第二座",
+        "desc_e": "Sai Kung Tower 2, Tseung Kwan O Plaza",
+        "date": "26/1/2020"
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [114.2605311, 22.3087575]
+    }
+},
+
+{
+    "type": "Feature",
+    "properties": {
+        "desc_c": "油尖旺區 香港 W 酒店",
+        "desc_e": "Yau Tsim Mong W Hong Kong",
+        "date": "28/1/2020"
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [114.1584803, 22.3049382]
+    }
+},
+
+{
+    "type": "Feature",
+    "properties": {
+        "desc_c": "中區 四季酒店",
+        "desc_e": "Central Four Seasons Hotel",
+        "date": "28/1/2020"
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [114.1584803, 22.3049382]
+    }
+},
+
+{
+    "type": "Feature",
+    "properties": {
+        "desc_c": "油尖旺區 御金‧國峰第二座",
+        "desc_e": "Yau Tsim Mong Tower 2, The Coronation",
+        "date": "30/1/2020"
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [114.1639563, 22.309285]
+    }
+},
+
+{
+    "type": "Feature",
+    "properties": {
+        "desc_c": "九龍城區 黃埔花園第十一期一座",
+        "desc_e": "Kowloon City Block 1, Site 11, Whampoa Garden",
+        "date": "31/1/2020"
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [114.1878109, 22.3046345]
+    }
+},
+
+];
+
+
---
+++ lang.js
@@ -0,0 +1,14 @@
+var langStore = {
+    "en": {
+      "welcome": "Welcome!",
+      "head_title": "Coronavirus Info.",
+      "home": "Home",
+      "links": "Links"
+    },
+    "tc": {
+      "welcome": "你好!",
+      "head_title": "武漢肺炎資訊",
+      "home": "主頁",
+      "links": "其他連結"
+    }
+}
