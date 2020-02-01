537bf69ba3a6782fc5f0f6b7c97c155e7eabbf46
Mike <hkmicc@gmail.com>
2020-02-01 21:56:18 +0800

    update

---
+++ data_os.js
@@ -0,0 +1,31 @@
+// https://www.chp.gov.hk/files/pdf/building_list_chi.pdf
+
+var outboundFeature = [
+
+{
+    "type": "Feature",
+    "properties": {
+        "desc_c": "深圳: 170 確診病例",
+        "desc_e": "Shenzhen: 170 confirmed cases",
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [114.0538691, 22.5538079]
+    }
+},
+
+{
+    "type": "Feature",
+    "properties": {
+        "desc_c": "澳門: 7 確診病例",
+        "desc_e": "Macau: 7 confirmed cases",
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [113.5001116, 22.161817]
+    }
+}
+
+];
+
+
