bf0b104e670c63d8eb80b0ed5dc8b37c024307f9
Mike <hkmicc@gmail.com>
2020-02-02 18:58:57 +0800

    update

--- data_os.js
+++ data_os.js
@@ -5,8 +5,20 @@ var outboundFeature = [
 {
     "type": "Feature",
     "properties": {
-        "desc_c": "深圳: 196 確診病例",
-        "desc_e": "Shenzhen: 196 confirmed cases",
+        "desc_c": "廣州: 179 確診病例",
+        "desc_e": "Guangzhou: 179 confirmed cases",
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [113.2612563, 23.1289093]
+    }
+},
+
+{
+    "type": "Feature",
+    "properties": {
+        "desc_c": "深圳: 207 確診病例",
+        "desc_e": "Shenzhen: 207 confirmed cases",
     },
     "geometry": {
         "type": "Point",
@@ -17,14 +29,54 @@ var outboundFeature = [
 {
     "type": "Feature",
     "properties": {
-        "desc_c": "澳門: 7 確診病例",
-        "desc_e": "Macau: 7 confirmed cases",
+        "desc_c": "澳門: 8 確診病例",
+        "desc_e": "Macau: 8 confirmed cases",
     },
     "geometry": {
         "type": "Point",
         "coordinates": [113.5001116, 22.161817]
     }
+},
+
+{
+    "type": "Feature",
+    "properties": {
+        "desc_c": "東莞: 27 確診病例",
+        "desc_e": "Dongguan: 27 confirmed cases",
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [113.74505996, 23.043088]
+    }
+},
+
+{
+    "type": "Feature",
+    "properties": {
+        "desc_c": "珠海: 47 確診病例",
+        "desc_e": "Zhuhai: 47 confirmed cases",
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [113.5683676, 22.2715044]
+    }
+},
+
+{
+    "type": "Feature",
+    "properties": {
+        "desc_c": "中山: 21 確診病例",
+        "desc_e": "Zhongshan: 21 confirmed cases",
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [113.3846472, 22.5171393]
+    }
 }
+
+
+
+
 
 ];
 
