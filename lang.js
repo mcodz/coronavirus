69c41f456084be7e992d1baaf880f78e27675643
Mike <hkmicc@gmail.com>
2020-02-02 19:29:43 +0800

    update

--- lang.js
+++ lang.js
@@ -2,12 +2,14 @@ var langStore = {
     "en": {
       "welcome": "Welcome!",
       "head_title": "Coronavirus Info.",
+      "head_subtitle": "Hong Kong and nearby areas",
       "home": "Home",
       "links": "Links"
     },
     "tc": {
       "welcome": "你好!",
       "head_title": "武漢肺炎資訊",
+      "head_subtitle": "香港及鄰近地區",
       "home": "主頁",
       "links": "其他連結"
     }
